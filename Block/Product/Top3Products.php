2319696c92add6a6d46e69fb4c47bc2ee3e9f730
Rostyslav Nazymko <rostislav.nazymko@symmetrics.de>
2017-01-11 15:09:10 +0200

    added block and template for widget

---
+++ Block/Product/Top3Products.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: naro
+ * Date: 1/11/17
+ * Time: 12:20 PM
+ */
+
+namespace Rostiknaz\Top3Widget\Block\Product;
+
+
+class Top3Products extends \Magento\Framework\View\Element\Template implements \Magento\Widget\Block\BlockInterface
+{
+    
+}
